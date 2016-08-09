5c22ff9843bc0247d6fc2139614f98e4800fd2a1
pecodrive <pecodrive@gmail.co>
2016-08-09 18:38:30 +0900

    cssfix

--- header.php
+++ header.php
@@ -10,6 +10,21 @@
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     </head>
+<?php if ( !is_user_logged_in() ){
+echo <<<aaa
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-36078315-10', 'auto');
+  ga('send', 'pageview');
+
+</script>
+aaa;
+}
+?>
     <body>
         <header>
             <div id="topwrap">
