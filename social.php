6ea0cd15876f4437d4d1c8aab8896df22bc7db25
pecodrive <pecodrive@gmail.co>
2016-08-09 17:48:06 +0900

    cssfix

--- social.php
+++ social.php
@@ -1,7 +1,9 @@
-<a href="<?php the_permalink(); ?>" class="hatena-bookmark-button" data-hatena-bookmark-layout="simple-balloon" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a>
+<ul class="sns"><li><a href="<?php the_permalink(); ?>" class="hatena-bookmark-button" data-hatena-bookmark-layout="simple-balloon" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a></li>
 
-<a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
+<li><a href="https://twitter.com/share" class="twitter-share-button">Tweet</a></li>
 
-<a data-pin-do="buttonBookmark" data-pin-lang="ja" data-pin-save="true" href="https://jp.pinterest.com/pin/create/button/"></a>
+<li><a data-pin-do="buttonBookmark" data-pin-lang="ja" data-pin-save="true" href="https://jp.pinterest.com/pin/create/button/"></a></li>
 
-<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-action="like" data-size="small" data-show-faces="false" data-share="true"></div>
+
+<li><div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-action="like" data-size="small" data-show-faces="false" data-share="true"></div></li>
+</ul>
