de6b5126b546c6af2a0cb9d6e70174bbe58bac7b
pecodrive <pecodrive@gmail.co>
2016-08-09 13:59:45 +0900

    cssfix

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <img src="./wp-content/themes/pyaka/img/footer.svg">
+    <img src="./wp-content/themes/pyakathema/img/footer.svg">
 </footer>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
