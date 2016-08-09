5c67c5d9e96b71bcbe82cc2a2c1c428463e5ba99
pecodrive <pecodrive@gmail.co>
2016-08-09 15:34:57 +0900

    cssfix

--- pagenation.php
+++ pagenation.php
@@ -4,7 +4,7 @@
         global $wp_query;
         $big = 999999999; 
         echo paginate_links( array(
-            'base' => str_replace( $big, '%_%', esc_url( get_pagenum_link( $big ) ) ),
+            'base' => 'http://pyaka.net/%_%',
             'format' => '?page=%#%',
             'current' => max( 1, get_query_var('paged') ),
             'total' => $wp_query->max_num_pages
