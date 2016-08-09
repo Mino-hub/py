83211811736dee41419b67f097e0168b7da63828
pecodrive <pecodrive@gmail.co>
2016-08-09 15:28:23 +0900

    cssfix

--- pagenation.php
+++ pagenation.php
@@ -3,18 +3,11 @@
     <?php
         global $wp_query;
         $big = 999999999; 
-        echo paginate_links( array(
+        paginate_links( array(
             'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
             'format' => '?page=%#%',
             'current' => max( 1, get_query_var('paged') ),
             'total' => $wp_query->max_num_pages
         ) );
-        $pagede = paginate_links( array(
-            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-            'format' => '?page=%#%',
-            'current' => max( 1, get_query_var('paged') ),
-            'total' => $wp_query->max_num_pages
-        ) );
-        var_dump($pagede);
     ?>
 </div>
