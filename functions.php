7c1c9c0ff7030ad373ab6f998b764a1503ed556a
pecodrive <pecodrive@gmail.co>
2016-09-16 12:38:59 +0900

    addsvg

--- functions.php
+++ functions.php
@@ -1,3 +1,8 @@
 <?php
 
 add_theme_support('post-thumbnails');
+function cc_mime_types($mimes) {
+      $mimes['svg'] = 'image/svg+xml';
+        return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
